80edc6aa772c7525e2585d70da075d97c1279481
Amanullah Aman <amancse.aiub@gmail.com>
2023-08-26 11:48:54 +0600

    nested loop in routes

---
+++ src/pages/EventsRoot.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+import EventsNavigation from "../components/EventsNavigation";
+import { Outlet } from "react-router-dom";
+
+const EventsRootLayout = () => {
+  return (
+    <Fragment>
+      <EventsNavigation />
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default EventsRootLayout;
